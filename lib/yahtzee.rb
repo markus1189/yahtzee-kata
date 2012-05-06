48e5dbc61e56c350ce1c31ef6aaed6a0e6edf74a
Markus Hauck <markus1189@gmail.com>
2012-05-06 16:40:55 +0200

    Refactored when clause to methods

--- lib/yahtzee.rb
+++ lib/yahtzee.rb
@@ -1,14 +1,26 @@
 class Yahtzee
+  def initialize dice_roll
+    @dice_roll = dice_roll
+  end
+
+  def method_missing meth, *args, &blk
+    if @dice_roll.respond_to? meth
+      @dice_roll.send meth, *args, &blk
+    else
+      super
+    end
+  end
+
   class << self
-    def [] category, dice
-      DiceRoll.new(dice).score(category)
+    def [] *dice
+      new DiceRoll.new(dice.flatten)
     end
   end
 end
 
 class DiceRoll
 
-  def initialize(*dice)
+  def initialize *dice
     @dice = dice.flatten.sort
 
     @categories = {
@@ -22,40 +34,53 @@ class DiceRoll
 
   end
 
-  def score category
-    case category
-    when /^ones$/, /^twos$/, /^threes$/, /^fours$/, /^fives$/, /^sixes$/
-      number = @categories.fetch(category.to_sym)
-      @dice.count(number) * number
-    when /^pair$/
-      return 0 unless pair?
-      @dice.partition { |num| @dice.count(num) >= 2 }.first.max * 2
-    when /^two pair$/
-      pairs = @dice.partition { |num| @dice.count(num) >= 2 }.first
-      if pairs.size == 4 then pairs.inject(&:+) else 0 end
-    when /^three of a kind$/
-      return 0 unless three?
-      @dice.max_by { |num| @dice.count num } * 3
-    when /^four of a kind$/
-      return 0 unless four?
-      @dice.partition { |num| @dice.count(num) == 4 }.first.inject(:+)
-    when /^small straight$/
-      return 0 unless straight? && [min,max] == [1,5]
-      15
-    when /^large straight$/
-      return 0 unless straight? && [min,max] == [2,6]
-      20
-    when /^full house$/
-      return 0 unless full_house?
-      @dice.inject(&:+)
-    when /^yahtzee$/
-      return 0 unless @dice.uniq.size == 1
-      50
-    when /^chance$/
-      @dice.inject(&:+)
-    else
-      fail "Unknown category: '#{category}'"
-    end
+  def score_numbered category
+    number = @categories.fetch(category.to_sym)
+    @dice.count(number) * number
+  end
+
+  def score_pair
+    return 0 unless pair?
+    pair_candidates = @dice.select { |num| @dice.count(num) >= 2 }.uniq
+    candidate = pair_candidates.max
+    2.times { @dice.delete candidate }
+    candidate * 2
+  end
+
+  def score_two_pairs
+    first  = score_pair
+    second = score_pair
+    if second == 0 then 0 else first + second end
+  end
+
+  def score_three_of_a_kind
+    x_of_a_kind(3)
+  end
+
+  def score_four_of_a_kind
+    x_of_a_kind(4)
+  end
+
+  def score_small_straight
+    score_straight(:small)
+  end
+
+  def score_large_straight
+    score_straight(:large)
+  end
+
+  def score_full_house
+    return 0 unless full_house?
+    @dice.inject(&:+)
+  end
+
+  def score_yahtzee
+    return 0 unless @dice.uniq.size == 1
+    50
+  end
+
+  def score_chance
+    sum
   end
 
   def pair?
@@ -88,7 +113,25 @@ class DiceRoll
     @dice.max
   end
 
+  def sum
+    @dice.inject(&:+)
+  end
+
   private
+
+  def score_straight(type)
+    max = if type == :small then 5 else 6 end
+    return 0 unless straight? && self.max == max
+    ((max-4)..max).inject(&:+)
+  end
+
+
+  def x_of_a_kind(x)
+    return 0 unless sequence_of? x
+    candidate = @dice.find { |num| @dice.count(num) == x }
+    x.times { @dice.delete candidate }
+    candidate * x
+  end
 
   def sequence_of? number
     @dice.any? { |num| @dice.count(num) == number }
--- spec/yahtzee_spec.rb
+++ spec/yahtzee_spec.rb
@@ -4,102 +4,102 @@ describe Yahtzee do
   context "scoring numbered categories" do
 
     it "should score the sum of the specified numbers" do
-      Yahtzee['fours', [1,1,2,4,4] ].should == 8
-      Yahtzee['ones',  [1,4,2,6,3] ].should == 1
-      Yahtzee['fives', [5,5,5,5,5] ].should == 25
+      Yahtzee[1,1,2,4,4].score_numbered('fours').should == 8
+      Yahtzee[1,4,2,6,3].score_numbered('ones').should == 1
+      Yahtzee[5,5,5,5,5].score_numbered('fives').should == 25
     end
 
   end
 
   context "scoring pairs" do
     it "should score the highest pair" do
-      Yahtzee['pair', [3,3,3,4,4] ].should == 8
+      Yahtzee[3,3,3,4,4].score_pair.should == 8
     end
 
     it "should score 0 if there is no pair" do
-      Yahtzee['pair', [3,1,2,5,4] ].should == 0
+      Yahtzee[3,1,2,5,4].score_pair.should == 0
     end
   end
 
   context "scoring two pairs" do
     it "should score both" do
-      Yahtzee['two pair', [1,1,2,3,3] ].should == 8
+      Yahtzee[1,1,2,3,3].score_two_pairs.should == 8
     end
 
     it "should score 0 if there is only one pair" do
-      Yahtzee['two pair', [5,5,1,2,3] ].should == 0
+      Yahtzee[5,5,1,2,3].score_two_pairs.should == 0
     end
 
     it "should score 0 if there is no pair" do
-      Yahtzee['two pair', [6,1,4,5,3] ].should == 0
+      Yahtzee[6,1,4,5,3].score_two_pairs.should == 0
     end
   end
 
   context "scoring three of a kind" do
     it "should score the sum of the three numbers" do
-      Yahtzee['three of a kind', [6,6,6,1,2] ].should == 18
+      Yahtzee[6,6,6,1,2].score_three_of_a_kind.should == 18
     end
 
     it "should score 0 if there are no three of a kind" do
-      Yahtzee['three of a kind', [1,6,6,1,2] ].should == 0
+      Yahtzee[1,6,6,1,2].score_three_of_a_kind.should == 0
     end
   end
 
   context "scoring four of a kind" do
     it "should score 4 times the number" do
-      Yahtzee['four of a kind', [1,4,4,4,4] ].should == 4*4
+      Yahtzee[1,4,4,4,4].score_four_of_a_kind.should == 4*4
     end
 
     it "should score 0 if there is no 'four of a kind'" do
-      Yahtzee['four of a kind', [1,4,4,2,4] ].should == 0
+      Yahtzee[1,4,4,2,4].score_four_of_a_kind.should == 0
     end
   end
 
   context "scoring a small straight" do
     it "should score 15" do
-      Yahtzee['small straight', [1,2,3,4,5] ].should == 15
+      Yahtzee[1,2,3,4,5].score_small_straight.should == 15
     end
 
     it "should score 0 if the dice are not a small straight" do
-      Yahtzee['small straight', [1,2,3,4,6] ].should == 0
+      Yahtzee[1,2,3,4,6].score_small_straight.should == 0
     end
   end
 
   context "scoring a large straight" do
     it "should score 15" do
-      Yahtzee['large straight', [2,3,4,5,6] ].should == 20
+      Yahtzee[2,3,4,5,6].score_large_straight.should == 20
     end
 
     it "should score 0 if the dice are not a large straight" do
-      Yahtzee['large straight', [1,2,3,4,5] ].should == 0
-      Yahtzee['large straight', [6,2,3,4,6] ].should == 0
+      Yahtzee[1,2,3,4,5].score_large_straight.should == 0
+      Yahtzee[6,2,3,4,6].score_large_straight.should == 0
     end
   end
 
   context "scoring a full house" do
     it "should score the sum of the dice" do
-      Yahtzee['full house', [1,1,1,6,6] ].should == 15
+      Yahtzee[1,1,1,6,6].score_full_house.should == 15
     end
 
     it "should score 0 if there is no full house" do
-      Yahtzee['full house', [5,1,1,6,6] ].should == 0
-      Yahtzee['full house', [3,3,3,3,3] ].should == 0
+      Yahtzee[5,1,1,6,6].score_full_house.should == 0
+      Yahtzee[3,3,3,3,3].score_full_house.should == 0
     end
   end
 
   context "scoring yahtzee" do
     it "should score 50" do
-      Yahtzee['yahtzee', [4,4,4,4,4] ].should == 50
+      Yahtzee[4,4,4,4,4].score_yahtzee.should == 50
     end
 
     it "should score 0 if there is no yahtzee" do
-      Yahtzee['yahtzee', [5,2,2,2,2] ].should == 0
+      Yahtzee[5,2,2,2,2].score_yahtzee.should == 0
     end
   end
 
   context "scoring change" do
     it "should score the sum of the dice" do
-      Yahtzee['chance', [1,4,4,2,1] ].should == 12
+      Yahtzee[1,4,4,2,1].score_chance.should == 12
     end
   end
 end
